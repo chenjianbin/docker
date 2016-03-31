db3f4e4defa7eadeb2de90db15a83c849c7fa87f
546391242@qq.com <root@localhost.localdomain>
2016-04-01 06:08:37 +0800

    update ssh_control.sh

--- run/ssh_control.sh
+++ run/ssh_control.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 case $1 in 
 "start")
-	docker run -d -p 50000:22 -v /data0/web/:/data0/web/ --name ssh_control 258.com/ssh_control 
+	docker run -d -p 50000:22 -v /data0/web/:/data0/web/ --name ssh_control registry.miguan.com/ssh_control 
 	;;
 "stop")
 	docker stop ssh_control
