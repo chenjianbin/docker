481404eb05b0a2f26b321328c4d1c28de509774b
chenjianbin <546391242@qq.com>
2016-06-20 11:45:44 +0800

    add php

---
+++ run/php.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+NAME=www.zsdb.com
+case $1 in 
+"start")
+	docker run -d -p 8001:80 -v /data0/web/$NAME:/data0/web/default -v /data0/logs/docker/$NAME:/data0/logs/ --name $NAME registry.miguan.com/php
+	;;
+"stop")
+	docker stop $NAME
+	docker rm $NAME
+	;;
+"restart")
+	$0 stop
+	$0 start
+	;;
+*)
+	echo "usage: $0 start|stop|restart"
+esac
